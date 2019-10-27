95f511937870658b921f087f40bc75c1644fa5b0
Omid Vahdaty <omidvd@gmail.com>
2019-10-27 11:32:41 +0200

    Update start_gcs_fuse.sh

--- airflow/setup/start_gcs_fuse.sh
+++ airflow/setup/start_gcs_fuse.sh
@@ -1,3 +1,3 @@
 echo mounting FUSE of GCS bucket airflow-fuse-investing -- logs and dags
-gcsfuse airflow-fuse-investing gs_logs
-gcsfuse airflow-fuse-investing gs_dags
+gcsfuse airflow-fuse-logs gs_logs
+gcsfuse airflow-fuse-dags gs_dags
