bdd10a18899f412670a5894194e04e625ed358f3
Omid Vahdaty <omidvd@gmail.com>
2019-12-17 15:48:06 +0200

    remove libmysqlclient-dev

--- airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
+++ airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-setuptools libmysqlclient-dev
+sudo apt-get -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-setuptools 
 
 sudo apt install -y python3-pip  python-boto3
 
