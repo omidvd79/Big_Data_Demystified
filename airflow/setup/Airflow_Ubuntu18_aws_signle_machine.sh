ff869f28af70511938f9afedcd79802e074c1d38
Omid Vahdaty <omidvd@gmail.com>
2019-12-17 15:45:24 +0200

    fix bug in packages

--- airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
+++ airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
@@ -2,13 +2,11 @@ sudo apt-get update
 
 sudo apt-get -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-setuptools 
 
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip libmysqlclient-dev python-boto3
+
+pip3 install pystan
 
 sudo pip3 install apache-airflow
-pip3 install pystan
-sudo apt install -y libmysqlclient-dev 
-
- sudo apt install -y python-boto3
 
 sudo -H pip3 install apache-airflow[all_dbs]
 
