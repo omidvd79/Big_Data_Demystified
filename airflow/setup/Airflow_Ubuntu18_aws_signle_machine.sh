f488b09c1264ea28d0360068d391f42ab3059cec
Omid Vahdaty <omidvd@gmail.com>
2019-12-17 16:59:27 +0200

    fix sasl error: libsasl2-dev

--- airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
+++ airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-setuptools default-libmysqlclient-dev libkrb5-dev 
+sudo apt-get -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-setuptools default-libmysqlclient-dev libkrb5-dev libsasl2-dev
 
 sudo apt install -y python3-pip  python-boto3
 
@@ -14,9 +14,7 @@ sudo pip3 install --upgrade setuptools
 sudo pip3 install apache-airflow
 
 sudo -H pip3 install apache-airflow[all_dbs]
-# error on pymssql/, will be fixed later.
 
 sudo -H pip3 install apache-airflow[devel]
 
 sudo pip3 install apache-airflow[all]
-#error on setup of kerberos, SASL. TBD, should be good to get started
