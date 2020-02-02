2dc7a0c2e6b57ff0d6f5a385f1c8c81e4453a0a2
Omid Vahdaty <omidvd@gmail.com>
2020-02-02 15:00:22 +0200

    run big query via python example & dataframe

---
+++ python_gcp_examples/run_query_on_bigquery_example.py
@@ -0,0 +1,31 @@
+import google.auth
+from google.cloud import bigquery
+from google.cloud import bigquery_storage_v1beta1
+
+# Explicitly create a credentials object. This allows you to use the same
+# credentials for both the BigQuery and BigQuery Storage clients, avoiding
+# unnecessary API calls to fetch duplicate authentication tokens.
+credentials, your_project_id = google.auth.default(
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
+)
+
+# Make clients.
+bqclient = bigquery.Client(
+    credentials=credentials,
+    project=your_project_id,
+)
+bqstorageclient = bigquery_storage_v1beta1.BigQueryStorageClient(
+    credentials=credentials
+)
+
+# Download query results.
+query_string = """
+ select * from t
+"""
+
+dataframe = (
+    bqclient.query(query_string)
+    .result()
+    .to_dataframe(bqstorage_client=bqstorageclient)
+)
+print(dataframe.head())
