bd365ef7edbf69ccd5d4cecc28db77a48878d93c
Omid Vahdaty <omidvd@gmail.com>
2020-03-18 22:14:24 +0200

    Create setup.py

---
+++ gce/setup.py
@@ -0,0 +1,11 @@
+# pip
+sudo apt update
+sudo apt install python-pip
+sudo apt install python3-pip
+
+# GCS fure
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update 
+sudo apt-get install -y gcsfuse
