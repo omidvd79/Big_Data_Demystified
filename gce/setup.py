71a6a17430019f6955fc8539dd7935803e00a0e5
Omid Vahdaty <omidvd@gmail.com>
2020-03-18 22:17:44 +0200

    Update setup.py

--- gce/setup.py
+++ gce/setup.py
@@ -3,6 +3,10 @@ sudo apt update
 sudo apt install -y python-pip
 sudo apt install -y python3-pip
 
+# pip packges:
+pip install google-cloud
+
+
 # GCS fuse
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
