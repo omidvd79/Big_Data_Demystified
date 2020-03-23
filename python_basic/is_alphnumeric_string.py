9f7a49bedc5df644a4855c37c1449a8bf7216f63
Omid Vahdaty <omidvd@gmail.com>
2020-03-23 12:27:07 +0200

    Update is_alphnumeric_string.py

--- python_basic/is_alphnumeric_string.py
+++ python_basic/is_alphnumeric_string.py
@@ -6,4 +6,4 @@ print('Is String Alphanumeric :', isalnum)
 
 alphanumeric_filter = filter(str.isalnum, a_string)
 alphanumeric_string = "".join(alphanumeric_filter)
-print(alphanumeric_string)
+print(alphanumeric_string.replace("/", ""))
