4678094b7d1e9e93fb34ec9ccfc03976c4a7551e
Omid Vahdaty <omidvd@gmail.com>
2020-03-23 20:41:55 +0200

    Update list_manipulation.py

--- python_basic/list_manipulation.py
+++ python_basic/list_manipulation.py
@@ -4,3 +4,16 @@ list.append("Big")
 list.append("Data")
 list.append("Demystified")
 print (list)
+
+#distinct object in a list 
+txt = "Big/Data/Big/Data/Big/Data"
+x = txt.split("/", 4)
+list=[]
+list.append(x[0])
+list.append(x[1])
+list.append(x[2])
+list.append(x[3])
+
+print (list)
+dist_list = set(list)
+print (dist_list)
