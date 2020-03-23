3711ae0a471f66409a0a1b3cfe73c758a2a0f38e
Omid Vahdaty <omidvd@gmail.com>
2020-03-23 20:23:37 +0200

    Update string_manipulations.py

--- python_basic/string_manipulations.py
+++ python_basic/string_manipulations.py
@@ -13,3 +13,10 @@ str ="Does Big Data acctuall mean Data Demystified?"
 #finding occurrences of 'Data'
 #in complete string
 print(str.count("Data"))
+
+#string split, and get first object in the list
+txt = "welcome/to/Big/Data/Demystified"
+x = txt.split("/", 1)
+print(x[0])
+
+
