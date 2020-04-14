3111ffe4bfccce3e50f84d07bbad4c02caf245d3
Omid Vahdaty <omidvd@gmail.com>
2020-04-14 13:53:15 +0300

    Update xcom_example_v4.py

--- airflow/examples/xcom_example_v4.py
+++ airflow/examples/xcom_example_v4.py
@@ -11,16 +11,12 @@ args = {
 
 dag = DAG('example_xcom_v4', schedule_interval=None, default_args=args, tags=['example'])
 
-t1 = BashOperator(
-    task_id="t1",
-    bash_command='echo "{{ ti.xcom_push(key="k1", value="v1") }}" "{{ti.xcom_push(key="k2", value="v2") }}"',
-    dag=dag,
-)
+t1 = BashOperator(task_id="t1",bash_command='echo "{{ ti.xcom_push(key="k1", value="v1") }}" "{{ti.xcom_push(key="k2", value="v2") }}"',dag=dag)
+t2 = BashOperator(task_id="t2",bash_command='echo "{{ ti.xcom_pull(key="k1") }}" "{{ ti.xcom_pull(key="k2") }}"',dag=dag)
 
-t2 = BashOperator(
-    task_id="t2",
-    bash_command='echo "{{ ti.xcom_pull(key="k1") }}" "{{ ti.xcom_pull(key="k2") }}"',
-    dag=dag,
-)
 
-t1 >> t2
+t3 = BashOperator(task_id='t3', xcom_push=True, bash_command="ls -la ; echo $?", dag=dag)
+t4 = BashOperator(task_id="t4",bash_command='echo "{{ ti.xcom_pull(task_ids="t3") }}"',dag=dag)
+
+
+t1 >> t2 >> t3 >> t4
