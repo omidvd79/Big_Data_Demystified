8613aea1f64075444dcdf8519c171a8d674ab87c
Omid Vahdaty <omidvd@gmail.com>
2020-05-12 18:10:52 +0300

    Update save_string_to_file.py

--- python_basic/save_string_to_file.py
+++ python_basic/save_string_to_file.py
@@ -1,7 +1,7 @@
 def save_to_file(data):
         start_date='1970-01-01'
         path="/tmp/myFile_"+start_date+".txt"
-        text_file = open("path", "w")
+        text_file = open(path, "w")
         n = text_file.write(json_data)
         text_file.close()
         print("file saved: "+path)
