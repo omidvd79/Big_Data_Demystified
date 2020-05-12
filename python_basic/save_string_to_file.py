6e19448936a55836df5455c6952dd4029a761213
Omid Vahdaty <omidvd@gmail.com>
2020-05-12 18:08:57 +0300

    Update save_string_to_file.py

--- python_basic/save_string_to_file.py
+++ python_basic/save_string_to_file.py
@@ -1,6 +1,6 @@
 def save_to_file(data):
         start_date='1970-01-01'
-        path="/tmp/facebook_marketing_api_"+start_date+".json"
+        path="/tmp/myFile_"+start_date+".txt"
         text_file = open("path", "w")
         n = text_file.write(json_data)
         text_file.close()
