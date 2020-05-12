4d20628dc3c518c1c0f192cf6112d0fe46f80f79
Omid Vahdaty <omidvd@gmail.com>
2020-05-12 18:08:33 +0300

    Update save_string_to_file.py

--- python_basic/save_string_to_file.py
+++ python_basic/save_string_to_file.py
@@ -1,3 +1,7 @@
-text_file = open("BigDataDemsytified.csv", "w")
-n = text_file.write('Welcome to Big Data Demystified')
-text_file.close()
+def save_to_file(data):
+        start_date='1970-01-01'
+        path="/tmp/facebook_marketing_api_"+start_date+".json"
+        text_file = open("path", "w")
+        n = text_file.write(json_data)
+        text_file.close()
+        print("file saved: "+path)
