26180c84e4fd4a89ae3867cc099446bb16d2e455
Omid Vahdaty <omidvd@gmail.com>
2020-07-19 22:15:19 +0300

    Update setup_ubuntu.sh

--- kylin/setup_ubuntu.sh
+++ kylin/setup_ubuntu.sh
@@ -1,4 +1,5 @@
-sudo apt-get install openssh-server
+sudo apt install -y build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+sudo apt-get install -y openssh-server
 sudo systemcel enable ssh
-sudo apt-get install net-tools
+sudo apt-get install -y net-tools
   
