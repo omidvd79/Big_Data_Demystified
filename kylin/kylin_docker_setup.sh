4b69ded2766e4747d6e8ef38de20929d6c399d85
Omid Vahdaty <omidvd@gmail.com>
2020-07-19 22:03:05 +0300

    Update kylin_docker_setup.sh

--- kylin/kylin_docker_setup.sh
+++ kylin/kylin_docker_setup.sh
@@ -1,1 +1,9 @@
-docker pull apachekylin/apache-kylin-standalone:3.1.0
+docker pull apachekylin/apache-kylin-standalone:3.1
+docker run -d \
+-p 7070:7070 \
+-p 8088:8088 \
+-p 50070:50070 \
+-p 8032:8032 \
+-p 8042:8042 \
+-p 16010:16010 \
+apachekylin/apache-kylin-standalone:3.1.0
