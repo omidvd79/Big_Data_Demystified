68968d9bd7671eda0d8fe9e635d778a0dfdb83b4
Omid Vahdaty <omidvd@gmail.com>
2020-07-19 22:47:01 +0300

    Update docker_load_tpch_to_hdfs.sh

--- kylin/docker_load_tpch_to_hdfs.sh
+++ kylin/docker_load_tpch_to_hdfs.sh
@@ -43,7 +43,7 @@ docker exec -it $1 hadoop fs -put /home/admin/customer.tbl /user/hive/warehouse/
 docker exec -it $1 hadoop fs -put /home/admin/orders.tbl /user/hive/warehouse/tpch/orders/
 
 
-#run hive create
+#run hive create lineitme
 docker exec -it $1 hive -e "create table lineitem (
     l_orderkey    BIGINT ,
     l_partkey     BIGINT ,
@@ -65,6 +65,94 @@ docker exec -it $1 hive -e "create table lineitem (
 row format delimited 
 fields terminated by '|' 
 lines terminated by '\n' 
-location '/user/hive/warehouse/tpch/lineitem/'
-tblproperties ("skip.header.line.count"="1");"
+location '/user/hive/warehouse/tpch/lineitem/';"
+
+#hiver create nation
+docker exec -it $1 hive -e "CREATE TABLE nation
+(
+   n_nationkey  INT  ,
+   n_name       CHAR(25) ,
+   n_regionkey  INT   ,
+   n_comment    VARCHAR(152)
+)
+row format delimited 
+fields terminated by '|' 
+lines terminated by '\n' 
+location '/user/hive/warehouse/tpch/nation/';"
+
+#hive create region
+docker exec -it $1 hive -e "CREATE TABLE region
+(
+   r_regionkey  INT  ,
+   r_name       CHAR(25)  ,
+   r_comment    VARCHAR(152)
+)row format delimited 
+fields terminated by '|' 
+lines terminated by '\n' 
+location '/user/hive/warehouse/tpch/region/'"
+
+#hive create part
+docker exec -it $1 hive -e "CREATE TABLE part
+(
+   p_partkey     BIGINT  ,
+   p_name        VARCHAR(55)  ,
+   p_mfgr        CHAR(25)  ,
+   p_brand       CHAR(10)  ,
+   p_type        VARCHAR(25)  ,
+   p_size        INT ,
+   p_container   CHAR(10)  ,
+   p_retailprice DOUBLE  ,
+   p_comment     VARCHAR(23)  
+)row format delimited 
+fields terminated by '|' 
+lines terminated by '\n' 
+location '/user/hive/warehouse/tpch/part/';"
+
+#hive create supplier
+docker exec -it $1 hive -e "CREATE TABLE supplier
+(
+   s_suppkey     BIGINT  ,
+   s_name        CHAR(25)  ,
+   s_address     VARCHAR(40) ,
+   s_nationkey   INT   ,
+   s_phone       CHAR(15)  ,
+   s_acctbal     DOUBLE  ,
+   s_comment     VARCHAR(101) 
+)row format delimited 
+fields terminated by '|' 
+lines terminated by '\n' 
+location '/user/hive/warehouse/tpch/supplier/';"
+
+#hive create customer
+docker exec -it $1 hive -e " CREATE TABLE customer
+(
+   c_custkey     BIGINT  ,
+   c_name        VARCHAR(25)  ,
+   c_address     VARCHAR(40)  ,
+   c_nationkey   INT ,
+   c_phone       CHAR(15)  ,
+   c_acctbal     DOUBLE  ,
+   c_mktsegment  CHAR(10)  ,
+   c_comment     VARCHAR(117) 
+)row format delimited 
+fields terminated by '|' 
+lines terminated by '\n' 
+location '/user/hive/warehouse/tpch/customer/'"
+
+#hive create orders
+docker exec -it $1 hive -e "CREATE TABLE orders
+(
+   o_orderkey       BIGINT  ,
+   o_custkey        BIGINT  ,
+   o_orderstatus    CHAR(1)  ,
+   o_totalprice     DOUBLE  ,
+   o_orderdate      DATE  ,
+   o_orderpriority  CHAR(15)  , 
+   o_clerk          CHAR(15)  ,
+   o_shippriority   INT ,
+   o_comment        VARCHAR(79)  
+)row format delimited 
+fields terminated by '|' 
+lines terminated by '\n' 
+location '/user/hive/warehouse/tpch/orders/'"
 
