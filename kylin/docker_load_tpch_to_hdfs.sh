6652c36681cbe5f84957eb577d89863b2b68d500
Omid Vahdaty <omidvd@gmail.com>
2020-07-19 22:28:57 +0300

    Update docker_load_tpch_to_hdfs.sh

--- kylin/docker_load_tpch_to_hdfs.sh
+++ kylin/docker_load_tpch_to_hdfs.sh
@@ -1,10 +1,12 @@
+echo "docker name is :"+$0
+
 sudo docker cp  /home/kyline/tpch-dbgen/lineitem.tbl  $0:/home/admin
-sudo docker cp  /home/kyline/tpch-dbgen/nation.tbl  $0:/home/admin
-sudo docker cp  /home/kyline/tpch-dbgen/region.tbl  $0:/home/admin
-sudo docker cp  /home/kyline/tpch-dbgen/part.tbl  $0:/home/admin
-sudo docker cp  /home/kyline/tpch-dbgen/supplier.tbl  $0:/home/admin
-sudo docker cp  /home/kyline/tpch-dbgen/customer.tbl  $0:/home/admin
-sudo docker cp  /home/kyline/tpch-dbgen/orders.tbl  $0:/home/admin
+sudo docker cp  /home/kylin/tpch-dbgen/nation.tbl  $0:/home/admin
+sudo docker cp  /home/kylin/tpch-dbgen/region.tbl  $0:/home/admin
+sudo docker cp  /home/kylin/tpch-dbgen/part.tbl  $0:/home/admin
+sudo docker cp  /home/kylin/tpch-dbgen/supplier.tbl  $0:/home/admin
+sudo docker cp  /home/kylin/tpch-dbgen/customer.tbl  $0:/home/admin
+sudo docker cp  /home/kylin/tpch-dbgen/orders.tbl  $0:/home/admin
 
 hadoop fs -mkdir /user/hive/warehouse/tpch/
 hadoop fs -mkdir /user/hive/warehouse/tpch/lineitem/
