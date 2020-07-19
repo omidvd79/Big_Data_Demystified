897abbe6f09b70b4472072e5f08371a3fa06c6ba
Omid Vahdaty <omidvd@gmail.com>
2020-07-19 22:16:03 +0300

    Update load_tpch.sh

--- kylin/load_tpch.sh
+++ kylin/load_tpch.sh
@@ -1,7 +1,7 @@
 sudo git clone https://github.com/electrum/tpch-dbgen.git
 cd tpch-dbgen/
 sudo apt install make
-makemak
+make
 ./dbgen -s 1 
 sudo docker ps
 echo "docker exec -it <container_id> bash"
