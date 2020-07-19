c7e25741d05b34ad973d64cf58e0e787a1eefdd9
Omid Vahdaty <omidvd@gmail.com>
2020-07-19 22:26:54 +0300

    Update docker_load_tpch_to_hdfs.sh

--- kylin/docker_load_tpch_to_hdfs.sh
+++ kylin/docker_load_tpch_to_hdfs.sh
@@ -1,3 +1,11 @@
+sudo docker cp  /home/kyline/tpch-dbgen/lineitem.tbl  $0:/home/admin
+sudo docker cp  /home/kyline/tpch-dbgen/nation.tbl  $0:/home/admin
+sudo docker cp  /home/kyline/tpch-dbgen/region.tbl  $0:/home/admin
+sudo docker cp  /home/kyline/tpch-dbgen/part.tbl  $0:/home/admin
+sudo docker cp  /home/kyline/tpch-dbgen/supplier.tbl  $0:/home/admin
+sudo docker cp  /home/kyline/tpch-dbgen/customer.tbl  $0:/home/admin
+sudo docker cp  /home/kyline/tpch-dbgen/orders.tbl  $0:/home/admin
+
 hadoop fs -mkdir /user/hive/warehouse/tpch/
 hadoop fs -mkdir /user/hive/warehouse/tpch/lineitem/
 hadoop fs -mkdir /user/hive/warehouse/tpch/nation/
