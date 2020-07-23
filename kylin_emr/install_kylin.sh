bb01d43f8e33b47c32898abe510dc0a1f9ad3779
Omid Vahdaty <omidvd@gmail.com>
2020-07-23 14:10:01 +0300

    Create install_kylin.sh

---
+++ kylin_emr/install_kylin.sh
@@ -0,0 +1,5 @@
+sudo mkdir /usr/local/kylin
+sudo chown hadoop /usr/local/kylin
+cd /usr/local/kylin
+wget https://apache.mivzakim.net/kylin/apache-kylin-3.1.0/apache-kylin-3.1.0-bin-hbase1x.tar.gz
+tar -xzvf apache-kylin-3.1.0-bin-hbase1x.tar.gz
