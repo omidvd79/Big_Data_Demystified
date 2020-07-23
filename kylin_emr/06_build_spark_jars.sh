67d60e825d4a6bc9e9f55b85551aa81309979eff
Omid Vahdaty <omidvd@gmail.com>
2020-07-23 15:54:49 +0300

    Update 06_build_spark_jars.sh

--- kylin_emr/06_build_spark_jars.sh
+++ kylin_emr/06_build_spark_jars.sh
@@ -1,11 +1,15 @@
-rm -rf $KYLIN_HOME/spark_jars
-mkdir $KYLIN_HOME/spark_jars
+#rm -rf $KYLIN_HOME/spark_jars
+mkdir -p $KYLIN_HOME/spark_jars
 cp /usr/lib/spark/jars/*.jar $KYLIN_HOME/spark_jars
 cp -f /usr/lib/hbase/lib/*.jar $KYLIN_HOME/spark_jars
 
 rm -f netty-3.9.9.Final.jar 
 rm -f netty-all-4.1.8.Final.jar
 
- jar cv0f spark-libs.jar -C $KYLIN_HOME/spark_jars .
+jar cv0f spark-libs.jar -C $KYLIN_HOME/spark_jars .
 ##aws s3 cp spark-libs.jar s3://{YOUR_BUCKET}/kylin/package/  # You choose s3 as your working-dir
-hadoop fs -put spark-libs.jar hdfs://kylin/package/  # You choose hdfs as your working-dir
+
+# if you use HDFS
+hadoop fs -mkdir /kylin/package 
+hadoop fs -put spark-libs.jar /kylin/package/
+
