a1f043b421f6b784b1946e8369f9027644cee405
Omid Vahdaty <omidvd@gmail.com>
2020-07-23 17:55:22 +0300

    Update 05_modify_bin_kylin.sh

--- kylin_emr/05_modify_bin_kylin.sh
+++ kylin_emr/05_modify_bin_kylin.sh
@@ -1,3 +1,4 @@
-#add this to bin/kylin.sh
+
+# sudo nano $KYLIN_HOME/bin/kylin.sh
 #export HBASE_CLASSPATH_PREFIX=${tomcat_root}/bin/bootstrap.jar:${tomcat_root}/bin/tomcat-juli.jar:${tomcat_root}/lib/*:$hive_dependency:$HBASE_CLASSPATH_PREFIX
  
