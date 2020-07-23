ba28987311a14869935e8d2f5d53fcf3babebfa0
Omid Vahdaty <omidvd@gmail.com>
2020-07-23 17:41:36 +0300

    Update 04_install_hadoop.sh

--- kylin_emr/04_install_hadoop.sh
+++ kylin_emr/04_install_hadoop.sh
@@ -19,5 +19,5 @@ rm -f netty-3.9.9.Final.jar
 rm -f netty-all-4.1.8.Final.jar
 
 jar cv0f spark-libs.jar -C $KYLIN_HOME/spark_jars .
-aws s3 cp spark-libs.jar s3://jutomate-kylin-hbase/kylin/package/  # You choose s3 as your working-dir
-#hadoop fs -put spark-libs.jar hdfs://kylin/package/  # You choose hdfs as your working-dir
+#aws s3 cp spark-libs.jar s3://jutomate-kylin-hbase/kylin/package/  # You choose s3 as your working-dir
+hadoop fs -put spark-libs.jar hdfs://kylin/package/  # You choose hdfs as your working-dir
