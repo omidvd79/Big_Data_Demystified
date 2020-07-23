d1a81c146d30d0f7eaf0dfad9477197fc96e09de
Omid Vahdaty <omidvd@gmail.com>
2020-07-23 18:49:51 +0300

    Update 04_install_hadoop.sh

--- kylin_emr/04_install_hadoop.sh
+++ kylin_emr/04_install_hadoop.sh
@@ -8,16 +8,16 @@ sudo -u hadoop hadoop fs -mkdir /kylin
 sudo -u hadoop hadoop fs -mkdir /kylin/package
 
 #Remove joda.jar
-sudo -u hadoop mv $HIVE_HOME/lib/jackson-datatype-joda-2.4.6.jar $HIVE_HOME/lib/jackson-datatype-joda-2.4.6.jar.backup
+sudo mv $HIVE_HOME/lib/jackson-datatype-joda-2.4.6.jar $HIVE_HOME/lib/jackson-datatype-joda-2.4.6.jar.backup
 
 # some more setting
-sudo -u hadoop rm -rf $KYLIN_HOME/spark_jars
-sudo -u hadoop mkdir -p $KYLIN_HOME/spark_jars
-sudo -u hadoop cp /usr/lib/spark/jars/*.jar $KYLIN_HOME/spark_jars
-sudo -u hadoop cp -f /usr/lib/hbase/lib/*.jar $KYLIN_HOME/spark_jars
+rm -rf $KYLIN_HOME/spark_jars
+mkdir -p $KYLIN_HOME/spark_jars
+cp /usr/lib/spark/jars/*.jar $KYLIN_HOME/spark_jars
+cp -f /usr/lib/hbase/lib/*.jar $KYLIN_HOME/spark_jars
 
-sudo -u hadoop rm -f netty-3.9.9.Final.jar 
-sudo -u hadoop rm -f netty-all-4.1.8.Final.jar
+rm -f netty-3.9.9.Final.jar 
+rm -f netty-all-4.1.8.Final.jar
 
 sudo -u hadoop jar cv0f spark-libs.jar -C $KYLIN_HOME/spark_jars .
 #aws s3 cp spark-libs.jar s3://jutomate-kylin-hbase/kylin/package/  # You choose s3 as your working-dir
