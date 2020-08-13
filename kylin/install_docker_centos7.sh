64686cd40edd8f23edf04eabe3f2cba3c49961e3
Omid Vahdaty <omidvd@gmail.com>
2020-08-13 14:40:23 +0300

    Update install_docker_centos7.sh

--- kylin/install_docker_centos7.sh
+++ kylin/install_docker_centos7.sh
@@ -1,3 +1,5 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
