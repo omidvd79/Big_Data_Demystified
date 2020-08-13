8d6ce206779efb6bb678bf0fe290aaab76c174da
Omid Vahdaty <omidvd@gmail.com>
2020-08-13 19:39:30 +0300

    Update install_airflow_centos7.sh

--- airflow/setup/install_airflow_centos7.sh
+++ airflow/setup/install_airflow_centos7.sh
@@ -1,6 +1,7 @@
 sudo yum install  -y mariadb-devel
 pip install wheel
 sudo pip3 install wheel
+sudo yum install -y gcc-c++
 
 #install python 3.7
 #https://github.com/omidvd79/Big_Data_Demystified/blob/master/linux/centos7/install_python3_7_cento7.sh
