ed8928ad1e98a3f928a70843291fff16a9644f3c
Omid Vahdaty <omidvd@gmail.com>
2020-08-13 19:05:34 +0300

    Update install_airflow_centos7.sh

--- airflow/setup/install_airflow_centos7.sh
+++ airflow/setup/install_airflow_centos7.sh
@@ -1,2 +1,14 @@
+sudo yum install  -y mariadb-devel
+
+#install python 3.7
+sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel wget
+cd ~
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+tar xzf Python-3.7.2.tgz
+cd Python-3.7.2.tgz
+./configure --enable-optimizations
+make altinstall
+
+
 sudo pip install apache-airflow
 sudo pip install apache-airflow[all]
