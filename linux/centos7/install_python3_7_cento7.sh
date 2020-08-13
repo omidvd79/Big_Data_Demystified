1247a7c39bb7466172bae2cd68effc5940ee907a
Omid Vahdaty <omidvd@gmail.com>
2020-08-13 21:12:53 +0300

    Update install_python3_7_cento7.sh

--- linux/centos7/install_python3_7_cento7.sh
+++ linux/centos7/install_python3_7_cento7.sh
@@ -2,7 +2,7 @@
 sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel wget
 cd ~/
 wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
-tar xzf Python-3.7.2.tgz
+tar xzf ~/Python-3.7.2.tgz
 cd Python-3.7.2.tgz
 ./configure --enable-optimizations
 sudo make altinstall
