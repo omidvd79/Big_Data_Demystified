112fff1107c85bc7c9ae677a10cc78a957fe9acf
Omid Vahdaty <omidvd@gmail.com>
2020-08-13 16:53:25 +0300

    Create add user.sh

---
+++ linux/centos7/add user.sh
@@ -0,0 +1,16 @@
+# add user cento7
+
+sudo adduser sqream
+sudo usermod -aG root sqream
+yum install -y nano
+
+#https://phoenixnap.com/kb/how-to-create-add-sudo-user-centos
+#sudo nano /etc/ssh/sshd_config
+#AllowUsers sqream
+#sudo systemctl restart sshd
+
+# add user to sudoers
+# https://phoenixnap.com/kb/how-to-create-add-sudo-user-centos
+# nano /etc/sudoers
+# sqream ALL=(ALL)        ALL
+
