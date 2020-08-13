aee43c0e4fb14f32e84aa95ebc67530d93a8ee2b
Omid Vahdaty <omidvd@gmail.com>
2020-08-13 19:17:06 +0300

    Update install_airflow_centos7.sh

--- airflow/setup/install_airflow_centos7.sh
+++ airflow/setup/install_airflow_centos7.sh
@@ -4,5 +4,5 @@ sudo yum install  -y mariadb-devel
 #https://github.com/omidvd79/Big_Data_Demystified/blob/master/linux/centos7/install_python3_7_cento7.sh
 
 #install airflow
-sudo pip install apache-airflow
-sudo pip install apache-airflow[all]
+sudo pip3 install apache-airflow
+sudo pip3 install apache-airflow[all]
