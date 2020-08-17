299b2f789676f0a6066531593670174fa7759bd1
Omid Vahdaty <omidvd@gmail.com>
2020-08-17 18:56:29 +0300

    Update setup_mysql_ubuntu20.sh

--- airflow/mysql_setup/setup_mysql_ubuntu20.sh
+++ airflow/mysql_setup/setup_mysql_ubuntu20.sh
@@ -1,2 +1,15 @@
 sudo apt update
 sudo apt install mysql-server
+# wizrad
+#sudo mysql_secure_installation
+# https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-20-04
+# https://linuxconfig.org/install-mysql-on-ubuntu-20-04-lts-linux
+sudo ufw allow from any to any port 3306 proto tcp
+
+#mysql client
+# 
+#SET GLOBAL validate_password.policy=LOW;
+#CREATE USER 'airflow'@'%' IDENTIFIED BY 'airflow';
+#CREATE DATABASE airflow;
+#GRANT ALL PRIVILEGES ON airflow.* TO 'airflow'@'%';
+
