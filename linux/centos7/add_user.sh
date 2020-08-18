f8645a55ce86547641df937e45b76bcaeaa06520
Omid Vahdaty <omidvd@gmail.com>
2020-08-18 11:12:46 +0300

    Update add_user.sh

--- linux/centos7/add_user.sh
+++ linux/centos7/add_user.sh
@@ -4,11 +4,6 @@ sudo adduser sqream
 sudo usermod -aG root sqream
 yum install -y nano
 
-#https://phoenixnap.com/kb/how-to-create-add-sudo-user-centos
-#sudo nano /etc/ssh/sshd_config
-#AllowUsers sqream - do not use this. this will not let u login to the machine and cause problems....
-#sudo systemctl restart sshd
-
 # add user to sudoers
 # https://phoenixnap.com/kb/how-to-create-add-sudo-user-centos
 # nano /etc/sudoers
