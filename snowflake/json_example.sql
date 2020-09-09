b1372864742c32453b6e1fc449c65fc24882a0fc
Omid Vahdaty <omidvd@gmail.com>
2020-09-09 15:22:20 +0300

    Update json_example.sql

--- snowflake/json_example.sql
+++ snowflake/json_example.sql
@@ -1,6 +1,6 @@
 create or replace stage MYS3STAGE2_json 
 url='s3://omid-simpo-test/json/' 
-CREDENTIALS=(aws_key_id='AKIAXVQKKZZNIFJ7Y7CW' aws_secret_key='/mlA42Do6EkJwDASsW+TVuyKGyNK2JV5L3lb0q6y') ;
+CREDENTIALS=(aws_key_id='AW' aws_secret_key='/ml6y') ;
 
 with data as(
 select $1 as v   from @MYS3STAGE2_json  (file_format => MY_JSON_FILE_FORMAT) limit 10
