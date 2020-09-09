533b19399c2ccb93b8b52f8cbd5346e3a7f48b4a
Omid Vahdaty <omidvd@gmail.com>
2020-09-09 14:44:10 +0300

    Create csv_example.sql

---
+++ snowflake/csv_example.sql
@@ -0,0 +1,10 @@
+list @MYS3STAGE
+
+create or replace stage MYS3STAGE2 
+url='s3://omid-simpo-test/csv/' 
+CREDENTIALS=(aws_key_id='AKIAXVQKKZZNIFJ7Y7CW' aws_secret_key='/mlA42Do6EkJwDASsW+TVuyKGyNK2JV5L3lb0q6y') ;
+
+select $1 as a,$2 as b,$3 as c from @MYS3STAGE2 (file_format => mys3csv) limit 10
+
+create table public.sample_csv2 as 
+select $1::int as a,$2::int as b,$3::int as c from @MYS3STAGE2 (file_format => mys3csv)  
