332c8d305c0a7db42495790dbd6af00c7602fe4b
Omid Vahdaty <omidvd@gmail.com>
2020-11-12 11:24:03 +0200

    Update Airflow_Ubuntu18_aws_signle_machine.sh

--- airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
+++ airflow/setup/Airflow_Ubuntu18_aws_signle_machine.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
 
+sudo apt -y install python-pip
+
 sudo apt-get -y install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev python-setuptools default-libmysqlclient-dev libkrb5-dev libsasl2-dev
 
 sudo apt install -y python3-pip  python-boto3
@@ -20,5 +22,7 @@ sudo -H pip3 install apache-airflow[all_dbs]
 sudo -H pip3 install apache-airflow[devel]
 
 sudo pip3 install apache-airflow[all]
+
+pip install botocore
 
 echo ***** "Now is the time you run: airflow initdb (if this is a first installation)"
