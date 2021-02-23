a8bccb7db4fb5640d13ae39719bfaa090730536a
Omid Vahdaty <omidvd@gmail.com>
2021-02-23 17:11:06 +0200

    Update aws_ubuntu20_airflow1_10_14.sh

--- airflow/setup/aws_ubuntu20_airflow1_10_14.sh
+++ airflow/setup/aws_ubuntu20_airflow1_10_14.sh
@@ -1,2 +1,6 @@
-sudo apt-get update
-sudo apt-get install build-essential
+sudo apt-get -y update
+sudo apt-get -y install build-essential
+#default python is 3.8.5
+sudo apt -y install python3-pip
+AIRFLOW_VERSION=1.10.14
+pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint  https://raw.githubusercontent.com/apache/airflow/constraints-1.10.14/constraints-3.8.txt
