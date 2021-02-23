7ba797b5183c221be9c848ae97f873b25d046b35
Omid Vahdaty <omidvd@gmail.com>
2021-02-23 17:13:11 +0200

    Update aws_ubuntu20_airflow1_10_14.sh

--- airflow/setup/aws_ubuntu20_airflow1_10_14.sh
+++ airflow/setup/aws_ubuntu20_airflow1_10_14.sh
@@ -4,3 +4,5 @@ sudo apt-get -y install build-essential
 sudo apt -y install python3-pip
 AIRFLOW_VERSION=1.10.14
 pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint  https://raw.githubusercontent.com/apache/airflow/constraints-1.10.14/constraints-3.8.txt
+echo "update bashrc to contain path to PATH=$PATH:~/.local/bin"
+
