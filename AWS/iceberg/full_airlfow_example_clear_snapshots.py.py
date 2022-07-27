550dfc1c86c1bacd648bdfea4ef95f76b29972f2
Omid Vahdaty <omidvd@gmail.com>
2022-07-27 11:22:39 +0300

    Create full_airlfow_example_clear_snapshots.py.py

---
+++ AWS/iceberg/full_airlfow_example_clear_snapshots.py.py
@@ -0,0 +1,111 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
+from airflow.utils.dates import days_ago
+
+DAG_ID = os.path.basename(__file__).replace('.py', '')
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+SPARK_STEPS = [
+    {
+        'Name': 'calculate_pi',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': ['spark-submit',
+                    '--conf', 'spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog',
+                    '--conf', 'spark.sql.catalog.demo.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog',
+                    '--conf', 'spark.sql.catalog.demo.warehouse=s3://Bucket, 
+                    '--conf', 'spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions',
+                    '--deploy-mode',
+                    'client','s3://bucket/emr_step/expire_snapshots.py'
+                    ],
+        },
+    }
+]
+
+JOB_FLOW_OVERRIDES = {
+    'Name': 'demo-cluster-airflow',
+    'ReleaseLabel': 'emr-6.6.0',
+    'Applications': [
+        {
+            'Name': 'Spark'
+        },
+    ],
+
+
+    'Configurations': [{"Classification":"iceberg-defaults","Properties":{"iceberg.enabled":"true"}}],
+
+
+    'Instances': {
+        'InstanceGroups': [
+            {
+                'Name': 'Master nodes',
+                'Market': 'ON_DEMAND',
+                'InstanceRole': 'MASTER',
+                'InstanceType': 'm5.xlarge',
+                'InstanceCount': 1,
+            }
+        ],
+        'KeepJobFlowAliveWhenNoSteps': False,
+        'TerminationProtected': False,
+    },
+    'VisibleToAllUsers': True,
+    'JobFlowRole': 'EMR_EC2_DefaultRole',
+    'ServiceRole': 'EMR_DefaultRole',
+    'Tags': [
+        {
+            'Key': 'Environment',
+            'Value': 'Development'
+        },
+        {
+            'Key': 'Name',
+            'Value': 'Airflow EMR Demo Project'
+        },
+        {
+            'Key': 'Owner',
+            'Value': 'Data Analytics Team'
+        }
+    ]
+}
+
+with DAG(
+        dag_id=DAG_ID,
+        description='Run built-in Spark app on Amazon EMR',
+        default_args=DEFAULT_ARGS,
+        dagrun_timeout=timedelta(hours=2),
+        start_date=days_ago(1),
+        schedule_interval='0 1 * * *',
+        tags=['emr'],
+) as dag:
+    cluster_creator = EmrCreateJobFlowOperator(
+        task_id='create_job_flow',
+        job_flow_overrides=JOB_FLOW_OVERRIDES
+    )
+
+    step_adder = EmrAddStepsOperator(
+        task_id='add_steps',
+        job_flow_id="{{ task_instance.xcom_pull(task_ids='create_job_flow', key='return_value') }}",
+        aws_conn_id='aws_default',
+        steps=SPARK_STEPS,
+    )
+
+    step_checker = EmrStepSensor(
+        task_id='watch_step',
+        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+        step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')[0] }}",
+        aws_conn_id='aws_default',
+    )
+
+    cluster_creator >> step_adder >> step_checker
