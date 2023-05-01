14d7045b49352dabf174265758008747f8eba6c6
Omid Vahdaty <omidvd@gmail.com>
2023-05-01 17:13:37 +0300

    Update DatabricksSubmitRunOperator_sql_adhoc.py

--- airflow/examples/DatabricksSubmitRunOperator_sql_adhoc.py
+++ airflow/examples/DatabricksSubmitRunOperator_sql_adhoc.py
@@ -4,21 +4,21 @@ from datetime import datetime, timedelta
 
 # Define the DAG
 default_args = {
-    'owner': 'airflow',
+    'owner': 'Jutomate',
     'depends_on_past': False,
     'start_date': datetime(2023, 5, 1),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
 dag = DAG(
-    'databricks_operator_example',
+    'DatabricksSubmitRunOperator_sql_example',
     default_args=default_args,
-    description='An example DAG using the Databricks operator',
-    schedule_interval=timedelta(hours=1)
+    description='DatabricksSubmitRunOperator',
+    schedule_interval=None
 )
 
 # Define the Databricks operator
@@ -26,8 +26,8 @@ databricks_operator = DatabricksSubmitRunOperator(
     task_id='databricks_operator_task',
     databricks_conn_id='databricks_default',
     new_cluster={
-        'spark_version': '8.4.x-scala2.12',
-        'node_type_id': 'i3.xlarge',
+        'spark_version': '7.3.x-scala2.12',
+        'node_type_id': 'n1-standard-4',#assuming GCP instances
         'num_workers': 1
     },
     spark_jar_task={
@@ -35,8 +35,9 @@ databricks_operator = DatabricksSubmitRunOperator(
         'parameters': [
             '--query', 'SELECT * FROM my_table LIMIT 100',
             '--output', 'my_table_output'
-        ]
+        ],
     },
+    libraries=[{"jar": "dbfs:/jutomate/job1.jar"}],
     dag=dag
 )
 
